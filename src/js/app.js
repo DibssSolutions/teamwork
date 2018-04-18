ed2ec31aaf1274534b269bbe70f3421d086d1cab
stasumanskiy <stasumanskiy@gmail.com>
2018-04-18 17:57:50 +0300

    add

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
 import svg4everybody from 'svg4everybody';
 
 svg4everybody();
+
+$('.header__length-ru').hover(function() {
+  $('.header__length-en').addClass('hover');
+}, function() {
+  $('.header__length-en').removeClass('hover');
+});
+
+
