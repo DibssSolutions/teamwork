8b729dd453bd8b6176face281c9e13c1ed0ae3f8
Lusjen <lusjenh@mail.ru>
2018-04-20 09:00:31 +0300

    add

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import svg4everybody from 'svg4everybody';
 import header from './lib/header.js';
+import './lib/valid-form.js';
 
 svg4everybody();
 
---
+++ src/js/lib/valid-form.js
@@ -0,0 +1,5 @@
+var name = $('.js-name');
+var phone = $('.js-phone');
+var error = $('.form__error');
+
+
