3459947b17b163becdbdf9a03158777b9c8b2d30
Lusjen <lusjenh@mail.ru>
2018-04-24 08:59:28 +0300

    add

--- src/js/lib/slider-client.js
+++ src/js/lib/slider-client.js
@@ -3,7 +3,12 @@ import 'slick-carousel';
 
 $('.js-slider-client').slick({
   dots: true,
+  dotsClass: 'slider__dots',
   prevArrow: '.slider__prev',
-  nextArrow: '.slider__next'
+  nextArrow: '.slider__next',
+  customPaging : function(slider, i) {
+    var title = $(slider.$slides[i]).find('[data-title]').data('title');
+    return '<a class="slider__item"> '+title+' </a>';
+  }
 });
 
